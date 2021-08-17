198782b5e9e2c2d2693725fc08cb1b8ac6ceb91e
Daniel Lewis <daniel.lewis@protonmail.com>
2021-08-17 13:40:27 -0600

    create seperate methods

--- Program.cs
+++ Program.cs
@@ -5,18 +5,37 @@ namespace CatWorx.BadgeMaker
 {
     class Program
     {
-        static void Main(string[] args)
+        static List<string> GetEmployees()
         {
-            List<string> employees = new List<string>() { "adam", "amy" };
+            // return a List of strings
+            List<string> employees = new List<string>();
+            // collect user values until the value is an empty string
+            while (true)
+            {
+                Console.WriteLine("Please enter a name: (leave empty to exit): ");
+                // Get a name from the console and assign it to a variable
+                string input = Console.ReadLine();
+                // break if the user hits ENTER without typing a name
+                if (input == "")
+                {
+                    break;
+                }
+                employees.Add(input);
+            }
+            return employees;
+        }
 
-            employees.Add("barbara");
-            employees.Add("billy");
-
+        static void PrintEmployees(List<string> employees)
+        {
             for (int i = 0; i < employees.Count; i++)
             {
                 Console.WriteLine(employees[i]);
             }
-
+        }
+        static void Main(string[] args)
+        {
+            List<string> employees = GetEmployees();
+            PrintEmployees(employees);
         }
     }
 }
