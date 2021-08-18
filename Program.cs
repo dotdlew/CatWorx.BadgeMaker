c62ff7956985c0879cc8a4a3664da80c74efdf8e
Daniel Lewis <daniel.lewis@protonmail.com>
2021-08-18 11:29:52 -0600

    added Util class

--- Program.cs
+++ Program.cs
@@ -34,19 +34,10 @@ namespace CatWorx.BadgeMaker
             }
             return employees;
         }
-        static void PrintEmployees(List<Employee> employees)
-        {
-            for (int i = 0; i < employees.Count; i++)
-            {
-                string template = "{0,-10}\t{1,-20}\t{2}";
-                // each item in employees is now an Employee instance
-                Console.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetName(), employees[i].GetPhotoUrl()));
-            }
-        }
         static void Main(string[] args)
         {
             List<Employee> employees = GetEmployees();
-            PrintEmployees(employees);
+            Util.PrintEmployees(employees);
         }
     }
 }
