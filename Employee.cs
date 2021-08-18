d0e8b29d81f35def23b3e7ae6c6570199dc510ea
Daniel Lewis <daniel.lewis@protonmail.com>
2021-08-17 20:41:18 -0600

    collect and print user input values

--- Employee.cs
+++ Employee.cs
@@ -6,15 +6,27 @@ namespace CatWorx.BadgeMaker
         public string LastName;
         public int Id;
         public string PhotoUrl;
-        public Employee(string firstName, string lastName)
+        public Employee(string firstName, string lastName, int id, string photoUrl)
         {
             FirstName = firstName;
             LastName = lastName;
+            Id = id;
+            PhotoUrl = photoUrl;
         }
 
         public string GetName()
         {
             return FirstName + " " + LastName;
+        }
+
+        public int GetId()
+        {
+            return Id;
+        }
+
+        public string GetPhotoUrl()
+        {
+            return PhotoUrl;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -12,28 +12,35 @@ namespace CatWorx.BadgeMaker
             // collect user values until the value is an empty string
             while (true)
             {
-                Console.WriteLine("Please enter a name: (leave empty to exit): ");
+                Console.WriteLine("Enter first name: (leave empty to exit): ");
                 // Get a name from the console and assign it to a variable
-                string input = Console.ReadLine();
+                string firstName = Console.ReadLine();
                 // break if the user hits ENTER without typing a name
-                if (input == "")
+                if (firstName == "")
                 {
                     break;
                 }
+                // get each value from user
+                Console.Write("Enter last name: ");
+                string lastName = Console.ReadLine();
+                Console.Write("Enter ID: ");
+                int id = Int32.Parse(Console.ReadLine());
+                Console.Write("Enter Photo URL:");
+                string photoUrl = Console.ReadLine();
                 // create new Employee instance
-                Employee currentEmployee = new Employee(input, "Lewis");
+                Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
                 // add currentEmployee
                 employees.Add(currentEmployee);
             }
             return employees;
         }
-
         static void PrintEmployees(List<Employee> employees)
         {
             for (int i = 0; i < employees.Count; i++)
             {
+                string template = "{0,-10}\t{1,-20}\t{2}";
                 // each item in employees is now an Employee instance
-                Console.WriteLine(employees[i].GetName());
+                Console.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetName(), employees[i].GetPhotoUrl()));
             }
         }
         static void Main(string[] args)
