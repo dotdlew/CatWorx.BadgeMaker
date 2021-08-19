65d490ed5ffc06b45b46ff38f2769fbec886765a
Daniel Lewis <daniel.lewis@protonmail.com>
2021-08-19 13:54:05 -0600

    establish peoplefetcher class

---
+++ PeopleFetcher.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+
+namespace CatWorx.BadgeMaker
+{
+    class PeopleFetcher
+    {
+        public static List<Employee> GetEmployees()
+        {
+            // return a List of Employee
+            List<Employee> employees = new List<Employee>();
+            // collect user values until the value is an empty string
+            while (true)
+            {
+                Console.WriteLine("Enter first name: (leave empty to exit): ");
+                // Get a name from the console and assign it to a variable
+                string firstName = Console.ReadLine();
+                // break if the user hits ENTER without typing a name
+                if (firstName == "")
+                {
+                    break;
+                }
+                // get each value from user
+                Console.Write("Enter last name: ");
+                string lastName = Console.ReadLine();
+                Console.Write("Enter ID: ");
+                int id = Int32.Parse(Console.ReadLine());
+                Console.Write("Enter Photo URL: ");
+                string photoUrl = Console.ReadLine();
+                // create new Employee instance
+                Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
+                // add currentEmployee
+                employees.Add(currentEmployee);
+            }
+            return employees;
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,40 +5,11 @@ namespace CatWorx.BadgeMaker
 {
     class Program
     {
-        static List<Employee> GetEmployees()
-        {
-            // return a List of Employee
-            List<Employee> employees = new List<Employee>();
-            // collect user values until the value is an empty string
-            while (true)
-            {
-                Console.WriteLine("Enter first name: (leave empty to exit): ");
-                // Get a name from the console and assign it to a variable
-                string firstName = Console.ReadLine();
-                // break if the user hits ENTER without typing a name
-                if (firstName == "")
-                {
-                    break;
-                }
-                // get each value from user
-                Console.Write("Enter last name: ");
-                string lastName = Console.ReadLine();
-                Console.Write("Enter ID: ");
-                int id = Int32.Parse(Console.ReadLine());
-                Console.Write("Enter Photo URL: ");
-                string photoUrl = Console.ReadLine();
-                // create new Employee instance
-                Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
-                // add currentEmployee
-                employees.Add(currentEmployee);
-            }
-            return employees;
-        }
         static void Main(string[] args)
         {
             List<Employee> employees = new List<Employee>();
 
-            employees = GetEmployees();
+            employees = PeopleFetcher.GetEmployees();
 
             Util.MakeCSV(employees);
             Util.MakeBadges(employees);
